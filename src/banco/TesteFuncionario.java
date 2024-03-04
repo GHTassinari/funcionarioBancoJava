e3016ad994751ac3637388397113d53c79383e1b
Guilherme Tassinari <102005103+GHTassinari@users.noreply.github.com>
2024-03-04 01:51:46 -0300

    Add files via upload

---
+++ src/banco/TesteFuncionario.java
@@ -0,0 +1,33 @@
+package banco;
+
+public class TesteFuncionario {
+
+    public static void main(String[] args) {
+
+
+        Funcionario f1 = new Funcionario("João Silva", "TI", 2500.0, "01/01/2023", "123456789");
+    /*
+        // Primeiro teste da classe funcionário
+        f1.bonifica(500.0); // Aumenta o salário em 500
+        f1.demite(); // Demite o funcionário, o tornando inativo
+        f1.mostra(); // Mostra todos os dados gerados, do funcionário
+
+    */
+
+        // Segundo teste da classe funcionário, do encapsulamento get e set
+        /*
+
+        f1.setNome("");
+
+        System.out.println("Nome: " + f1.getNome());
+        System.out.println("Departamento: " + f1.getDepartamento());
+        System.out.println("Salário: R$" + f1.getSalario());
+        System.out.println("Data de Entrada: " + f1.getDataEntrada());
+        System.out.println("RG: " + f1.getRg());
+        System.out.println("Ativo: " + (f1.isAtivo() ? "Sim" : "Não"));
+
+         */
+
+    }
+
+}
